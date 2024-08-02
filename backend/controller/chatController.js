b3dbcbebc92ea7944d3f589e3024e271275e33b4
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-08-02 19:53:29 +0530

    Add Conversation backend

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -1,6 +1,7 @@
 import { Chat } from "../models/chat.js";
+import { Conversation } from "../models/conversation.js";
 
-export const createChat = async(req, res)=>{
+export const createChat = async (req, res) => {
     try {
         const userId = req.user._id;
 
@@ -10,31 +11,54 @@ export const createChat = async(req, res)=>{
         res.status(201).json(chat);
     } catch (error) {
         res.status(500).json({
-            message:error.message,
+            message: error.message,
         })
     }
 }
 
-export const getAllChats = async(req, res)=>{
+export const getAllChats = async (req, res) => {
     try {
-        const chats = await Chat.find({user: req.user._id}).sort({
+        const chats = await Chat.find({ user: req.user._id }).sort({
             createdAt: -1,
         });
 
         res.status(200).json(chats);
     } catch (error) {
         res.status(500).json({
-            message:error.message,
+            message: error.message,
         })
     }
 }
 
-export const conversation = async(req, res)=>{
+export const addConversation = async (req, res) => {
     try {
-        const chat = 
+        const chat = await Chat.findById(req.params.id);
+
+        if (!chat) {
+            return res.status(404).json({
+                message: "No chats found with this id",
+            })
+        }
+
+        const conversation = await Conversation.create({
+            chat: chat._id,
+            question: req.body.question,
+            answer: req.body.answer,
+        })
+
+        const updateChat = await Chat.findByIdAndUpdate(req.params.id,
+            { latestMessage: req.body.answer, },
+            { new: true, });
+
+
+        res.status(200).json({
+            conversation, 
+            updateChat
+        })
+
     } catch (error) {
         res.status(500).json({
-            message:error.message,
+            message: error.message,
         })
     }
 }
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { isAuth } from '../middleware/isAuth.js';
-import { createChat, getAllChats } from '../controller/chatController.js';
+import { addConversation, createChat, getAllChats } from '../controller/chatController.js';
 
 const router = express.Router();
 
 router.post('/new', isAuth, createChat);
 
 router.get('/all', isAuth, getAllChats);
+
+router.post('/:id', isAuth, addConversation)
 
 export default router;
