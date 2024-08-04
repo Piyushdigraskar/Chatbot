8afbe7e8a7a5d79afd77598449fd242332c5cdd8
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-08-04 16:30:50 +0530

    Creating and adding sideBar to the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import {useState} from 'react'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleSidebar = ()=>{
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className='text-white'>Home</div>
+    <div className='flex h-screen bg-gray-900 text-white'>
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+      <div className="flex flex-1 flex-col"></div>
+    </div>
   )
 }
 
