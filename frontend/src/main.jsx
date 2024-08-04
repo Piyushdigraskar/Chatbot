94370e43695d9f4f54cbfb35ecf2518c796e3bac
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-08-05 01:57:52 +0530

    Revert "Adding gemini Api"
    This reverts commit e006f716f0e41c9b10701879ee1126b5586f9296.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { UserProvider } from './context/userContext.jsx'
-import { ChatProvider } from './context/chatContext.jsx'
 
 
 export const server = "http://localhost:5000";
@@ -11,9 +10,7 @@ export const server = "http://localhost:5000";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <UserProvider>
-      <ChatProvider>
-        <App />
-      </ChatProvider>
+      <App />
     </UserProvider>
   </React.StrictMode>,
 )
