823475e1dae35b35f1906ccaf784b21c6338406a
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-08-05 12:59:18 +0530

    Getting chats from backend to side bar

--- frontend/src/context/chatContext.jsx
+++ frontend/src/context/chatContext.jsx
@@ -1,5 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import axios from 'axios';
+import { server } from "../main";
+import toast from "react-hot-toast";
 
 const ChatContext = createContext();
 
@@ -7,6 +9,8 @@ export const ChatProvider = ({ children }) => {
     const [messages, setMessages] = useState([]);
     const [prompt, setPrompt] = useState("");
     const [newReqLoading, setNewReqLoading] = useState(false);
+    const [chats, setChats] = useState([]);
+    const [createLoad, setCreateLoad] = useState(false)
 
     async function fetchResponse() {
         if (prompt === "") return alert('Write a prompt');
@@ -26,7 +30,7 @@ export const ChatProvider = ({ children }) => {
                 answer: response["data"]["candidates"][0]["content"]["parts"][0]["text"],
             }
 
-            setMessages((prev)=>[...prev, message]);
+            setMessages((prev) => [...prev, message]);
             setNewReqLoading(false);
         } catch (error) {
             alert("Something went wrong")
@@ -35,7 +39,40 @@ export const ChatProvider = ({ children }) => {
         }
     }
 
-    return <ChatContext.Provider value={{fetchResponse, messages, prompt, setPrompt, newReqLoading}}>
+    async function fetchChats() {
+        try {
+            const { data } = await axios.get(`${server}/api/chat/all`, {
+                headers: {
+                    token: localStorage.getItem('token'),
+                }
+            })
+
+            setChats(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async function createChat() {
+        setCreateLoad(true);
+        try {
+            const { data } = await axios.post(`${server}/api/chat/new`, {}, {
+                headers: {
+                    token: localStorage.getItem("token"),
+                }
+            })
+            fetchChats();
+            setCreateLoad(false);
+        } catch (error) {
+            toast.error("Something went wrong");
+            setCreateLoad(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchChats();
+    }, [])
+    return <ChatContext.Provider value={{ fetchResponse, messages, prompt, setPrompt, newReqLoading, chats, createChat, createLoad }}>
         {children}
     </ChatContext.Provider>
 }
