da861053153c1a1e4f76e403bbb2ec3068c027f6
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-08-06 10:43:33 +0530

    Full pre Deploymeent installtion

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDb from './database/db.js';
 import cors from 'cors';
+import path from 'path';
 
 
 dotenv.config();
+
+const __dirname = path.resolve();
 const app = express();
 
 //using middleware
@@ -17,6 +20,12 @@ import chatRoutes from './routes/chatRoutes.js';
 //using routes
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
+
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'))
+})
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is working on port ${process.env.PORT}`)
